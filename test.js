02bf7e6dba75c42c33c4afe3884c3ed8fc2aa8be
GatereKinyanjui <gaterekinyanjui@gmail.com>
2022-09-30 17:57:40 +0300

    checkwinner added

--- test.js
+++ test.js
@@ -23,34 +23,19 @@ function getComputerChoice(arr) {
 
 
 function singleRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         console.log("it's a tie! try again.");
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection !== "paper") {
-        console.log("you lose! paper beats rock.");
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        console.log("you win! rock crushes scissors");
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        console.log("you win! paper beats rock.");
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        console.log("you lose! scissors butcher paper");
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        console.log("you lose! rock crushes scissors");
-    } else (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper");
-    console.log("you win! scissors butcher paper");
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice(arr);
-console.log(singleRound(playerSelection, computerSelection));
-
-
-
-
-// 3. play multiple rounds
-function game() {
-    for (let i = 0; i < arr.length; i++) {
-        console.log(singleRound(playerSelection, computerSelection))
+    }
+    if ((playerSelection.toLowerCase() === "rock" && computerSelection !== "paper") ||
+        (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") ||
+        (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock")) {
+        console.log("you lose!");
+    }
+    if ((playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") ||
+        (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ||
+        (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")) {
+        console.log("you win!");
     }
 }
 
-console.log(game());
+
